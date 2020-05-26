589dc9cdda5d59f03bb03502c535e14fe29d9683
Leandro Gabatel <legabatel@hotmail.com>
2020-05-26 19:19:08 -0300

    AjusteDeRoutas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     tokenKey: "loginFootAPIToken",
     authKey: "authKey",
     production: true,
-    urlAPIFoot: 'http://201.75.200.31:120/api',
-    urlAPIAuth: 'http://201.75.200.31:120/api/auth'
+    urlAPIFoot: 'http://footunity.ddns.net/api',
+    urlAPIAuth: 'http://footunity.ddns.net/api/auth'
 };
